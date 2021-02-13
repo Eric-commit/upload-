cc0f351fa233673b42c8e1eb88c4fa4c240c8b2d
Eric <ericgoncalves232@gmail.com>
2021-02-13 09:55:35 -0300

    end stats

---
+++ stats.js
@@ -0,0 +1,20 @@
+setInterval(() => {
+    const os = require('os')
+
+    const { freemem, totalmem } = os
+
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem() / 1024 / 1024)
+    const percents = parseInt((mem / total) * 100)
+
+    const stats = {
+        free: `${mem} MB`,
+        total: `${total} MB`,
+        usage: `${percents}%`
+    }
+
+    console.clear()
+    console.log("****** status do PC ******")
+    console.table(stats)
+
+}, 1000)
