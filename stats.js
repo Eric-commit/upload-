6637ad371efac8787d1936fd436e0fea6d503cac
Eric <ericgoncalves232@gmail.com>
2021-02-18 15:59:54 -0300

    end logger

--- stats.js
+++ stats.js
@@ -1,5 +1,7 @@
+const os = require('os')
+const log = require('./logger')
+
 setInterval(() => {
-    const os = require('os')
 
     const { freemem, totalmem } = os
 
@@ -16,5 +18,7 @@ setInterval(() => {
     console.clear()
     console.log("****** status do PC ******")
     console.table(stats)
+
+    log(`${JSON.stringify(stats)}\n`)
 
 }, 1000)
